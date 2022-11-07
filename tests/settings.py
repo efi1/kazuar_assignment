681a49d097ae1425419587a5579c1db2a410390e
efi1 <efovadia@gmail.com>
2022-11-07 23:29:25 +0200

    Update settings.py

--- tests/settings.py
+++ tests/settings.py
@@ -1,9 +1,7 @@
 url = "https://www.tutorialspoint.com/linux_terminal_online.php"
-readme_text = "README.txt edited at its end"
+readme_text = "README.txt"
 target_folder = "F'{Path(__file__).parent.parent}{os.sep}target'"
 target_file = "F'{Path(__file__).parent.parent}{os.sep}target{os.sep}data.json'"
 chrome_driver_path = "F'{Path(__file__).parent.parent}{os.sep}drivers{os.sep}chromedriver.exe'"
 params_test_path = "F'{Path(__file__).parent.parent}{os.sep}config_tests{os.sep}test_cat_options.json'"
 is_run_silently = ''
-
-
